52e7482290fc94cf37b289f1e7c558d4bf1cc5c8
Fabiola <suarezfabiola17@gmail.com>
2024-10-03 12:28:27 -0300

    Se cambia donde se pone el firebaseConfig

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyC0BsCRH_rYfEDUo_Tl9y10LI5sHstgXjU',
+    authDomain: 'pruebaapp-c4d44.firebaseapp.com',
+    projectId: 'pruebaapp-c4d44',
+    storageBucket: 'pruebaapp-c4d44.appspot.com',
+    messagingSenderId: '441368942075',
+    appId: '1:441368942075:web:bc051e980417f6f26ee75e',
+  },
 };
 
 /*
--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,6 @@ import { environment } from './environments/environment';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
-import { environmentFs } from './environments/environmentConfig';
 import { provideAnimations } from '@angular/platform-browser/animations';
 
 if (environment.production) {
@@ -24,7 +23,7 @@ bootstrapApplication(AppComponent, {
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     provideIonicAngular(),
     provideRouter(routes),
-    provideFirebaseApp(() => initializeApp(environmentFs)),
+    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
     provideAuth(() => getAuth()),
     provideFirestore(() => getFirestore()),
     provideAnimations(),
