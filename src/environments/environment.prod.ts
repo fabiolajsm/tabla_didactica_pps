2182cf32b81dee476b4832ce29ee0c393f6d2089
Fabiola <suarezfabiola17@gmail.com>
2024-10-04 11:39:01 -0300

    Agrego firebaseConfig a environment pro

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyC0BsCRH_rYfEDUo_Tl9y10LI5sHstgXjU',
+    authDomain: 'pruebaapp-c4d44.firebaseapp.com',
+    projectId: 'pruebaapp-c4d44',
+    storageBucket: 'pruebaapp-c4d44.appspot.com',
+    messagingSenderId: '441368942075',
+    appId: '1:441368942075:web:bc051e980417f6f26ee75e',
+  },
 };
